f7aa64bdfc8131127263b8884ee95db4882eac38
Sudeep Prasad <sudeep.rp@gmail.com>
2024-06-20 16:05:35 +0530

    pass one

--- stats.cpp
+++ stats.cpp
@@ -1,5 +1,24 @@
 #include "stats.h"
 
-Stats Statistics::ComputeStatistics(const std::vector<___>& ) {
-    //Implement statistics here
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <cmath>
+
+using namespace Statistics;
+
+Stats Statistics::ComputeStatistics(const std::vector<float>& data) {
+    Stats stats;
+
+    if (data.empty()) {
+        stats.average = 0.0f;
+        stats.min = 0.0f;
+        stats.max = 0.0f;
+    } else {
+        stats.average = std::accumulate(data.begin(), data.end(), 0.0f) / data.size();
+        stats.min = *std::min_element(data.begin(), data.end());
+        stats.max = *std::max_element(data.begin(), data.end());
+    }
+
+    return stats;
 }
